1c2c60a6ba1542664d5c5e4a8ac3e760ccfc41bd
lcy <33@qq.com>
2023-04-29 12:08:47 +0800

    da

---
+++ src/main/java/cn/tedu/petstoreleader/PetStoreLeaderApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.petstoreleader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreLeaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetStoreLeaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tedu/petstoreleader/PetStoreLeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.petstoreleader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreLeaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
